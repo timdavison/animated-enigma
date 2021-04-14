03f000385ede99fc8870577bbe64ab93fd16a2a9
Tim Davison <tpd28@cam.ac.uk>
2021-04-14 15:47:43 +0100

    Add some Material components

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,12 @@
 import * as React from "react"
+import { AppBar, Toolbar } from "@material-ui/core";
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
+    <AppBar>
+      <Toolbar>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -27,7 +18,8 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
+      </Toolbar>
+    </AppBar>
   </header>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 
 import Header from "./header"
 import "./layout.css"
@@ -26,24 +21,20 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
+
+        <Container maxWidth='lg'>
+          <Box component='main'>{children}</Box>
+          <footer
+           style={{
             marginTop: `2rem`,
-          }}
-        >
+            }}
+          >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
-      </div>
+        </Container>
+
     </>
   )
 }
