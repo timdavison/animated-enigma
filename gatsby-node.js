ca2a8b0f979730cb9c22493cef5d01bd634d7ea5
Tim Davison <tpd28@cam.ac.uk>
2021-04-19 18:54:32 +0100

    Use slug rather thanid for page urls

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,37 @@
  */
  const path = require(`path`)
 
+ function slugify(string) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
+
+
+module.exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions
+
+    if (node.internal.type === "node__stories") {
+        const slug = node.path.alias ? node.path.alias : ("/story/" + slugify(node.title));
+
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug
+        })
+    }
+}
+
+
  exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
 
@@ -13,15 +44,23 @@
       allNodeStories {
         edges {
           node {
+            title
             id
+            path {
+              alias
+            }
+            fields {
+              slug
+            }
           }
         }
       }
     }
   `).then(result => {
     result.data.allNodeStories.edges.forEach(({ node }) => {
+      console.log(node);
       createPage({
-        path: node.id,
+        path: node.path.alias ? node.path.alias : ("/story/" + slugify(node.title)),
         component: path.resolve(`./src/templates/story.js`),
         context: {
           id: node.id,
