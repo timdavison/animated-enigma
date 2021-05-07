2de5fc6527477c0307493b98f37a9389865b921c
Tim Davison <tpd28@cam.ac.uk>
2021-05-07 09:43:42 +0100

    fix error when single image is null

--- src/components/ContentSingleImageItem.js
+++ src/components/ContentSingleImageItem.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function ContentSingleImageItem({value}) {
-  const image = getImage(value.relationships.field_paragraphs_single_image.relationships.field_media_image.localFile.childImageSharp.gatsbyImageData);
-  return <GatsbyImage image={image} alt=' ' />
+
+  if(value.relationships.field_paragraphs_single_image) {
+    const image = getImage(value.relationships.field_paragraphs_single_image.relationships.field_media_image.localFile.childImageSharp.gatsbyImageData);
+    return <GatsbyImage image={image} alt=' ' />
+  }
+
+  return (null);
 }
