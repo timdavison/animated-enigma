ee04d11edfa8ac36c373c9f17fbe0e1cf72f2793
Tim Davison <tpd28@cam.ac.uk>
2021-05-10 14:23:34 +0100

    remove console.log

--- src/components/DrupalImage.js
+++ src/components/DrupalImage.js
@@ -3,7 +3,6 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function DrupalImage({imageField}) {
 
-  console.log(imageField)
   if(imageField) {
     const image = getImage(imageField.relationships.field_media_image.localFile.childImageSharp.gatsbyImageData);
     return <GatsbyImage image={image} alt=' ' />
