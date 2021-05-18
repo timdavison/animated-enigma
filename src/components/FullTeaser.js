1b12b4ee8c1cd889b361eb195e54a15fdd4b4a58
Tim Davison <tpd28@cam.ac.uk>
2021-05-18 11:08:19 +0100

    Trim summary on full teaser

--- src/components/FullTeaser.js
+++ src/components/FullTeaser.js
@@ -3,13 +3,14 @@ import * as React from "react"
 import DrupalImage from './DrupalImage';
 
 export default function FullTeaser({image, slug, title, created, summary}) {
-
+  const rawSummary = (summary) ? summary : "This story has no summary";
+  const summaryTrimmed = (rawSummary.length <= 300) ? rawSummary : rawSummary.slice(0,300) + "...";
   return (
     <>
       <h3><Link to={ slug }>{ title }</Link></h3>
       <DrupalImage imageField={image} alt=' ' />
       <p><small><em>date goes here</em></small></p>
-      <p>{summary}</p>
+      <p>{summaryTrimmed}</p>
     </>
   )
 }
