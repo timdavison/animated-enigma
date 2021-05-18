132ac97054b098bb90fe6048ae13e19e733b8eee
Tim Davison <tpd28@cam.ac.uk>
2021-05-18 11:25:37 +0100

    output date in full teaser

--- src/components/FullTeaser.js
+++ src/components/FullTeaser.js
@@ -5,11 +5,12 @@ import DrupalImage from './DrupalImage';
 export default function FullTeaser({image, slug, title, created, summary}) {
   const rawSummary = (summary) ? summary : "This story has no summary";
   const summaryTrimmed = (rawSummary.length <= 300) ? rawSummary : rawSummary.slice(0,300) + "...";
+  const createdDate = new Date(created);
   return (
     <>
       <h3><Link to={ slug }>{ title }</Link></h3>
       <DrupalImage imageField={image} alt=' ' />
-      <p><small><em>date goes here</em></small></p>
+      <p><small><em>{createdDate.toDateString()}</em></small></p>
       <p>{summaryTrimmed}</p>
     </>
   )
